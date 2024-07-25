da0cd878a3f73c4abdfb63c1b035902c7158dad7
rupatul1995 <rupalichavan578@gmail.com>
2024-07-25 21:36:10 +0530

    update

---
+++ TEST-4/index.js
@@ -0,0 +1,69 @@
+// Question 1.Write a JavaScript for loop that iterates from 0 to 20 and prints only the even numbers,
+//  but skips the number 10.
+
+
+
+//  var num=20;
+
+// for( var i=0; i<=20;i++){
+//     if(i %2===0 && i!=10){
+//       console.log("The even number",i);
+//     }
+//    }
+  
+  
+  //  Q.2  Write a JavaScript while loop that calculates the factorial of a given number and also counts how many iterations were needed.
+  //  pennding
+  
+  
+  
+  // Q .3Create a JavaScript function that takes an array of numbers as input and returns the sum of all the even
+  //  numbers using a for loop.
+
+//   var array=[1,2,3,4,5,6,7,8,9,10];
+//   let sum=0;
+//   function myData(array){
+  
+//   for(var i=0; i<10; i++){
+//     if(array[i]%2===0 ){
+//       sum +=array[i];
+//     //   console.log(sum,"sum");
+//     }
+   
+//   }
+//   return sum;
+// }
+// const finalsum=myData(array);
+// console.log("sum of all even number", finalsum);
+
+
+
+// Q .5Create a JavaScript function that takes an array of strings as input
+//  and returns a new array with only the strings that have a length greater than 7 characters.
+
+// function myData(array){
+ 
+//     return array.filter=myData(str=>str.length>7);
+
+// }
+// var array2=["Rohit", "Rushikesh", "Nilam", "Jitesh"];
+// const finalarray=myData(array2);
+// console.log(finalarray);
+
+
+// Question 7:
+
+// Create a JavaScript program that calculates the sum of all numbers from 1 to 1000 that are divisible by 3, 5, or 7.
+
+// function divisibleNumber(){
+//     sum=0;
+//      for( var i=1; i<=1000;i++){
+//         if(i%3===0  && i%5===0 && i%7===0){
+//             sum+=i;
+//         }
+//      }
+//      return sum;
+
+// }
+// const finalresult=divisibleNumber()
+//  console.log("sum of all numbers  =",finalresult );
